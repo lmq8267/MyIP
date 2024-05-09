a6ad659fb15c66da3ffb97bf96eec35dd678f4c0
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-05-09 13:09:56 +0800

    Update ipsb.js

--- api/ipsb.js
+++ api/ipsb.js
@@ -16,9 +16,9 @@ export default (req, res) => {
 
     if (referer) {
         const domain = new URL(referer).hostname;
-        if (!allowedDomains.includes(domain)) {
-            return res.status(403).json({ error: 'Access denied' });
-        }
+        //if (!allowedDomains.includes(domain)) {
+            //return res.status(403).json({ error: 'Access denied' });
+        //}
     } else {
         return res.status(403).json({ error: 'What are you doing?' });
     }
